8ea8a53e7c811eba8886841f3cd0889011f66768
haybuka <paschal.nduleo@gmail.com>
2023-11-04 01:51:22 +0100

    cleanup

--- app/component/navigation/mobile.tsx
+++ app/component/navigation/mobile.tsx
@@ -5,6 +5,7 @@ import styles from './navigation.module.css';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import IconType from '../icon/iconType';
+import { navigation, socials } from '@/utils/content';
 
 const MobileNavigation = () => {
   const [visible, setVisible] = useState(false);
@@ -40,45 +41,33 @@ const MobileNavigation = () => {
           visible && `translate-x-0`
         )}
       >
-        <Link
-          href={'/home'}
-          className={cls(
-            styles.link,
-            ` ${pathname === '/home' && styles.active_link}`
-          )}
-        >
-          <span>00</span>
-          <span className={styles.divider}></span>
-          <span>Work</span>
-        </Link>
-        <Link
-          href={'/article'}
-          className={cls(
-            styles.link,
-            ` ${pathname === '/article' && styles.active_link}`
-          )}
-        >
-          <span>01</span>
-          <span className={styles.divider}></span>
-          <span>Article</span>
-        </Link>
-        <Link
-          href={'https://linkedin.com/in/paschal-ndulue-336980134'}
-          className={cls(
-            styles.link,
-            ` ${pathname === '' && styles.active_link}`
-          )}
-        >
-          <span>02</span>
-          <span className={styles.divider}></span>
-          <span>Linked In</span>
-        </Link>
+        {navigation.map((navigate, id) => {
+          return (
+            <Link
+              key={id}
+              href={navigate.link}
+              className={cls(
+                styles.link,
+                ` ${
+                  pathname.toLowerCase() === navigate.link.toLowerCase() &&
+                  styles.active_link
+                }`
+              )}
+            >
+              <span>0{id}</span>
+              <span className={styles.divider}></span>
+              <span>{navigate.name}</span>
+            </Link>
+          );
+        })}
         <aside className="w-[40%]">
           <p className="my-6">Connect With Me</p>
           <div className="flex items-center justify-between ">
-            <IconType icon="twitter" size={5} />
-            <IconType icon="github" size={5} />
-            <IconType icon="mail" size={5} />
+            {socials.map((social, id) => (
+              <Link key={id} href={social.link} target="_blank">
+                <IconType icon={social.icon} size={5} />
+              </Link>
+            ))}
           </div>
         </aside>
       </section>
